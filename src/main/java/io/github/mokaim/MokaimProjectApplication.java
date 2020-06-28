943d91167b5d6a96088d9205476ce80889c51336
mokaims <mokaim@outlook.com>
2020-06-28 14:40:29 +0900

    first commit

---
+++ src/main/java/io/github/mokaim/MokaimProjectApplication.java
@@ -0,0 +1,13 @@
+package io.github.mokaim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokaimProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MokaimProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mokaim/MokaimProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mokaim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MokaimProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
