ca492b93263506549f8924bb4e72db47cb9ded76
mokaims <mokaim@outlook.com>
2020-07-03 03:20:50 +0900

    add hikari cp

---
+++ src/main/java/io/github/mokaim/config/DataSourceConfig.java
@@ -0,0 +1,31 @@
+package io.github.mokaim.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@PropertySource("classpath:/application.properties")
+public class DataSourceConfig {
+	
+	
+	@Bean
+	@ConfigurationProperties(prefix="spring.datasource.hikari")
+	public HikariConfig hikariConfig() {
+		return new HikariConfig();
+	}
+	
+	@Bean
+	public DataSource datasource() throws Exception{
+		DataSource dataSource = new HikariDataSource(hikariConfig());
+		System.out.println(dataSource.toString());
+		return dataSource;
+	}
+
+}
--- src/test/java/io/github/mokaim/MokaimProjectApplicationTests.java
+++ src/test/java/io/github/mokaim/MokaimProjectApplicationTests.java
@@ -1,13 +1,17 @@
 package io.github.mokaim;
 
+
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class MokaimProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("dddd");
 	}
 
 }
